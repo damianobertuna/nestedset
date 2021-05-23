27af8603d385859b1330d83bda48f972e2915eb2
Damiano Bertuna <damianobertuna@gmail.com>
2021-05-23 11:30:13 +0200

    Changed comments

--- api.php
+++ api.php
@@ -9,21 +9,20 @@ header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json; charset=UTF-8");
 
 try {
-    /* clase per interagire con il database, causa una eccezione se
-    * la connessione fallisce e il flusso si ferma */
+    /* clase per interagire con il database */
     $db             = new Database($user, $password, $dbname, $host);
 
     /* $responseObj - inizializzo i valori della struttura da ritornare come risposta */
     $responseObj    = new responseClass(0, array(), 0, 0, "");
 
-    /* $requestData - validiamo i parametri passati tramite GET
-    * se la validazione fallisce viene generata una eccezione e il flusso si ferma */
+    /* $requestData - validiamo i parametri passati tramite GET */
     $requestData    = new requestData($_GET, $responseObj);
+
     /* $nestedObj - oggetto tramite il quale ricerco i figli del nodo passato tramite GET
     * in base agli altri parametri di filtro (language, search_keyword, page_num e page_size */
     $nestedObj      = new nestedSet($db, $requestData, $responseObj);
 
-    /* il metodo children cerca i nodi figli e ritorna la stringa json creata*/
+    /* il metodo children cerca i nodi figli e ritorna la stringa json generata */
     $response       = $nestedObj->findChildren();
 
     echo $response;
